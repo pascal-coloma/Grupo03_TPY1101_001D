d06626105c7fef081c76a8452f2261e8b953b49e
luuuuuur <plazasjolifenzio@gmail.com>
2026-04-18 16:02:31 -0400

    fixing up setup.sh indentation bug

--- backend/imSystem/scripts/setup.sh
+++ backend/imSystem/scripts/setup.sh
@@ -13,32 +13,33 @@ if [ ! -d "/home/ubuntu/backend/imSystem" ]; then
     echo "ERROR: carpeta /home/ubuntu/backend/imSystem no existe"
     exit 1
 fi
+
 #==SECCION INSTALLS===
 echo "===UPDATING REPOSITORIES==="
 sudo apt update
-#==SECCION INSTALLS===
+
 echo "===SETING UP NGINX==="
-if [ ! -f "/usr/sbin/nginx" ];then
+if [ ! -f "/usr/sbin/nginx" ]; then
     sudo apt install nginx -y
     echo "Ingresa la IP o DNS del servidor:"
     read SERVER_IP
-    sudo tee /etc/nginx/conf.d/ims.conf > /dev/null<<EOF
-    server {
-        listen 80;
-        server_name $SERVER_IP;
+    sudo tee /etc/nginx/conf.d/ims.conf > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $SERVER_IP;
 
-        location / {
-            proxy_pass http://127.0.0.1:8000;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        }
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
+}
 EOF
-
     sudo rm -f /etc/nginx/sites-enabled/default
     sudo rm -f /etc/nginx/sites-available/default
 fi
+
 sudo systemctl daemon-reload
 sudo systemctl start nginx
 sudo systemctl status nginx
@@ -51,29 +52,32 @@ cd /home/ubuntu/backend
 if [ ! -d "/home/ubuntu/backend/env" ]; then
     python3 -m venv env
 fi
+
 echo "===UPDATING DEPENDENCIES==="
 /home/ubuntu/backend/env/bin/pip install -r /home/ubuntu/backend/install.txt
+
 if [ ! -f "/etc/systemd/system/gunicorn.service" ]; then
     echo "===SETTING UP GUNICORN==="
-        sudo tee /etc/systemd/system/gunicorn.service > /dev/null<< EOF
-        [Unit]
-        Description=Gunicorn IMS
-        After=network.target
+    sudo tee /etc/systemd/system/gunicorn.service > /dev/null <<EOF
+[Unit]
+Description=Gunicorn IMS
+After=network.target
 
-        [Service]
-        User=ubuntu
-        WorkingDirectory=/home/ubuntu/backend/imSystem
-        EnvironmentFile=/home/ubuntu/backend/imSystem/.mikufile
-        ExecStart=/home/ubuntu/backend/env/bin/gunicorn \\
-            backend_config.wsgi:application \\
-            --bind 127.0.0.1:8000 \\
-            --workers 3
-        Restart=always
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu/backend/imSystem
+EnvironmentFile=/home/ubuntu/backend/imSystem/.mikufile
+ExecStart=/home/ubuntu/backend/env/bin/gunicorn \\
+    backend_config.wsgi:application \\
+    --bind 127.0.0.1:8000 \\
+    --workers 3
+Restart=always
 
-        [Install]
-        WantedBy=multi-user.target
+[Install]
+WantedBy=multi-user.target
 EOF
 fi
+
 echo "===STARTING GUNICORN==="
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
