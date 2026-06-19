f140e71a3473f3ad69d6f8b57c36783e13bd93e5
luuuuuur <plazasjolifenzio@gmail.com>
2026-06-19 01:22:26 -0400

    BACKENBD

--- producto/backend/imSystem/ims_backend/s3.py
+++
@@ -1,6 +0,0 @@
-# s3.py
-import boto3
-from django.conf import settings
-from botocore.config import Config
-
-s3_client = boto3.client('s3', region_name=settings.AWS_S3_REGION_NAME, config=Config(signature_version='s3v4'))
