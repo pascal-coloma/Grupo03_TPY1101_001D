ae9a2ff9e5f2c37f44fa9618d38053c05b79a9dc
luuuuuur <plazasjolifenzio@gmail.com>
2026-06-19 23:27:03 -0400

    Solucion de bug no agregado backend

--- producto/backend/backend/imSystem/ims_backend/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-class ImsBackendConfig(AppConfig):
-    name = 'ims_backend'
-    def ready(self):
-        from ims_backend.aws_package.secrets_manager import Secrets
-        self.secrets_aws = Secrets._secrets
-        
